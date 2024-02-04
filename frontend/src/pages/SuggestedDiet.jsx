b4f0bfae863e1c080810a586783f575dbdd315b2
meetg21 <94597802+meetg21@users.noreply.github.com>
2024-02-04 08:47:48 +0530

    suggested diet added

--- frontend/src/pages/SuggestedDiet.jsx
+++ frontend/src/pages/SuggestedDiet.jsx
@@ -1,24 +1,30 @@
 // SuggestedDiet.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SuggestedDiet = () => {
-  console.log(localStorage.getItem("userinfo"));
+  const userInfo = JSON.parse(localStorage.getItem("userinfo"));
+//   console.log(userInfo);
+//   const dietRecommendation = userInfo.dietRecommendation;
+//   console.log(dietRecommendation);
+const [diet, setDiet] = useState([]);
+
+
+  // Log the extracted dietRecommendation
+//   console.log(dietRecommendation);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-4">
       <h1 className="text-2xl md:text-4xl font-bold text-blue-500 mb-8">
         Suggested Diet made for you by Healthify
+        {/* {userInfo.data.dietRecommendation} */}
+
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Breakfast Card */}
         <div className="bg-white rounded-lg shadow-lg p-2 flex flex-col items-center">
           <h2 className="text-lg font-bold mb-2">Breakfast</h2>
-          <img
-            src="" // Replace with your dish image URL
-            alt="Breakfast"
-            className="w-24 h-24 object-cover rounded-full mb-2"
-          />
+
           <p className="text-center mb-1">Dish Name</p>
           <div className="flex space-x-2">
             <button className="border border-black font-bold px-2 py-1">
@@ -36,11 +42,7 @@ const SuggestedDiet = () => {
           className="bg-white rounded-lg shadow-lg p-2 flex flex-col items-center"
         >
           <h2 className="text-lg font-bold mb-2">Lunch</h2>
-          <img
-            src="" // Replace with your dish image URL
-            alt="Lunch"
-            className="w-24 h-24 object-cover rounded-full mb-2"
-          />
+
           <p className="text-center mb-1">Dish Name</p>
           <div className="flex space-x-2">
             <button className="border border-black font-bold px-2 py-1">
@@ -55,11 +57,7 @@ const SuggestedDiet = () => {
         {/* Dinner Card */}
         <div className="bg-white rounded-lg shadow-lg p-2 flex flex-col items-center">
           <h2 className="text-lg font-bold mb-2">Dinner</h2>
-          <img
-            src="" // Replace with your dish image URL
-            alt="Dinner"
-            className="w-24 h-24 object-cover rounded-full mb-2"
-          />
+
           <p className="text-center mb-1">Dish Name</p>
           <div className="flex space-x-2">
             <button className="border border-black font-bold px-2 py-1">
